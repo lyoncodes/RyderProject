7f7a0395e7aedd9c4d8a13d80dbd4c25a8706585
Michael Lyon <lyoncodes@gmail.com>
2022-01-10 13:41:01 -0800

    add headers, header styles, header js

---
+++ main.js
@@ -0,0 +1,4 @@
+// create a new variable named heading
+// assign it the html element with the id 'heading'
+var heading = document.getElementById('heading');
+heading.textContent = 'Sucka MCs Should Call Me Sire';
